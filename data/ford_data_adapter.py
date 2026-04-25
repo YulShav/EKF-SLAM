0789988c0f25895aa0b0fa0a29594132d10dad26
Yulia <m2300747@edu.misis.ru>
2026-04-25 23:21:20 +0300

    add adapter.. with some mistakes

--- data/ford_data_adapter.py
+++ data/ford_data_adapter.py
@@ -3,50 +3,37 @@ from pathlib import Path
 from rosbags.rosbag1 import Reader
 from rosbags.typesys import Stores, get_typestore
 
-def load_data(bag_path):
-    bag_path = Path(bag_path)
-    typestore = get_typestore(Stores.LATEST)
+def get_u(vx, vy, omega_z, dt):
+    dx = vx * dt
+    dy = vy * dt
+    dtheta = omega_z * dt
 
-    timestamps = []
-    us = []
-    zs = []
+    return np.array([dx, dy, dtheta])
 
-    last_time = None
-    last_vx = None
 
-    with Reader(bag_path) as reader:
-        velocity = None
-        ground_truth = None
-        
-        for connection in reader.connections:
-            if connection.topic == '/velocity_raw':
-                velocity = connection
-            elif connection.topic == '/pose_ground_truth':
-                ground_truth = connection
+class FordDataAdapter:
+    def __init__(self, bag_path: Path):
+        self.bag_path = Path(bag_path)
+        self.typestore = get_typestore(Stores.LATEST)
+        self.last_ts = None
 
-        for connection, timestamp, data in reader.messages():
-            msg = typestore.deserialize(data, connection.msgtype)
-
-            if connection.topic == '/velocity_raw':
-                vx = msg.vector.x
-
-                if last_time is not None:
-                    dt = timestamp - last_time
-                    dx = vx * dt
-                    dy = 0.0
-                    dtheta = 0.0
-                    
-                    u = np.array([dx, dy, dtheta])
-                    us.append(u)
-                    timestamps.append(timestamp)
+    def __iter__(self):
+        with Reader(self.bag_path) as reader:
+            for connection, timestamp, data in reader.messages():
+                msg = self.typestore.deserialize(data, connection.msgtype)
                 
-                last_time = timestamp
-                last_vx = vx
-
-            elif connection.topic == '/pose_ground_truth':
-                x = msg.pose.position.x
-                y = msg.pose.position.y
-                z = np.array([x, y, 0.0])
-                zs.append(z)
-
-    return timestamps, us, zs
+                if connection.topic == '/velocity_raw':
+                    vx = msg.vector.x
+                    vy = msg.vector.y
+                    self.last_vx = vx
+                    self.last_vy = vy
+                    
+                elif connection.topic == '/imu':
+                    omega_z = msg.angular_velocity.z
+                    
+                    if self.last_ts is not None:
+                        dt = timestamp - self.last_ts
+                        u = get_u(self.last_vx, self.last_vy, omega_z, dt)
+                        yield 'predict', u, timestamp
+                    
+                    self.last_ts = timestamp
